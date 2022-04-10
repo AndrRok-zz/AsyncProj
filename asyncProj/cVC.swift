bb9264be88f5121d23258690924551697457a506
AndrRok <rokhmanov2000@yandex.ru>
2022-04-10 19:59:00 +0400

    first commit

---
+++ asyncProj/cVC.swift
@@ -0,0 +1,29 @@
+//
+//  cVC.swift
+//  asyncProj
+//
+//  Created by Andrey Rokhmanov on 05.04.2022.
+//
+
+import UIKit
+
+class cVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
